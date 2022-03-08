2c794b9c564edfb92daa588c93642b02de7cf45f
Hassoun <hassoun.k@mobiarchitects.com>
2022-03-08 18:02:48 +0100

    Add MaterialPicker

---
+++ RHMobile/RHMobile/Controls/MaterialPicker.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XForms.Controls
+{
+    public partial class MaterialPicker : StackLayout
+    {
+        public static readonly BindableProperty DateProperty =
+            BindableProperty.Create(nameof(Date), typeof(string), typeof(View), String.Empty, BindingMode.TwoWay);
+
+        public string Date
+        {
+            get { return (string)GetValue(DateProperty); }
+            set { SetValue(DateProperty, value); }
+        }
+
+        public static readonly BindableProperty LeftGlyphProperty =
+         BindableProperty.Create(nameof(LeftGlyph), typeof(string), typeof(View), string.Empty, BindingMode.TwoWay);
+
+        public string LeftGlyph
+        {
+            get { return (string)GetValue(LeftGlyphProperty); }
+            set
+            {
+                SetValue(LeftGlyphProperty, value);
+            }
+        }
+
+
+        public MaterialPicker()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- RHMobile/RHMobile/Resources/Values/FontAwesomeFonts.cs
+++ RHMobile/RHMobile/Resources/Values/FontAwesomeFonts.cs
@@ -5,11 +5,16 @@ namespace XForms.Resources
     {
         public const string FontFamilySolid = "FontAwesome5Solid";
         public const string FontFamilyRegular = "FontAwesome5Regular";
-        public const string FontAwesome5Brands = "FontAwesome5Brands";
+        public const string FontFamilyBrands = "FontAwesome5Brands";
     }
 
     public static partial class FontAwesomeFonts
     {
         public const string Bell = "\uf0f3";
+        public const string Calendar = "\uf073";
+        public const string AngleDown = "\uf107";
+
+
+
     }
 }
